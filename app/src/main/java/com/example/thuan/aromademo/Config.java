971f0f69b1a2e88c785515999066e80b6b4bd5db
thuan-PC\thuan <thuandh@kaopiz.com>
2016-12-25 10:05:04 -0800

    code library

---
+++ app/src/main/java/com/example/thuan/aromademo/Config.java
@@ -0,0 +1,9 @@
+package com.example.thuan.aromademo;
+
+/**
+ * Created by thuan on 12/24/2016.
+ */
+
+public class Config {
+    public static final String GITHUB_URL = "https://github.com/hoang8f/android-flat-button";
+}
---
+++ app/src/main/java/com/example/thuan/aromademo/MaterialEditextActivity.java
@@ -0,0 +1,13 @@
+package com.example.thuan.aromademo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MaterialEditextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material_editext);
+    }
+}
